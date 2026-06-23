df33f0925058583d24d512e2b5bff48c3a4be0ac
Vitor Henrique <117254147+AnG3L0ftHeN1GhT@users.noreply.github.com>
2026-06-23 16:54:15 -0300

    fp

--- MLP_Unity/Assets/Scripts/PlayerController.cs
+++ MLP_Unity/Assets/Scripts/PlayerController.cs
@@ -11,10 +11,16 @@ public class PlayerController : MonoBehaviour
     public CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
+    private Transform cameraTransform;
 
     [Header("Input Actions")]
     public InputActionReference moveAction;
     public InputActionReference jumpAction;
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+    }
 
     private void OnEnable()
     {
@@ -42,10 +48,8 @@ public class PlayerController : MonoBehaviour
         // Read input
         Vector2 input = moveAction.action.ReadValue<Vector2>();
         Vector3 move = new Vector3(input.x, 0, input.y);
-        move = Vector3.ClampMagnitude(move, 1f);
-
-        if (move != Vector3.zero)
-            transform.forward = move;
+        move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
+        // move = Vector3.ClampMagnitude(move, 1f);
 
         // Jump using WasPressedThisFrame()
         if (groundedPlayer && jumpAction.action.WasPressedThisFrame())
