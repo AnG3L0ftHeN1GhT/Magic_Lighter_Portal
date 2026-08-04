e00d275b4906270670cf62f61d10b28bf09db76a
samuel-huntrix-fan6667-decimator-of-slug <mrjuniorbetinha@gmail.com>
2026-08-04 14:01:03 -0300

    nada tem uns modelo adicionado :)

---
+++ MLP_Unity/Assets/Scripts/CandleManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CandleManager : MonoBehaviour
+{
+    public GameObject green1;
+    public GameObject green2;
+    public GameObject purple1;
+    public GameObject purple2;
+    public GameObject gold1;
+    public GameObject gold2;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ MLP_Unity/Assets/Scripts/velas.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class velas : MonoBehaviour
+{
+    public string cor;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+        //ligar velinhas
+        if (cor=="roxo")
+        {
+            //instanciar particula1
+        }
+        else if (cor=="dourado")
+        {
+            //instanciar particula2
+        }
+        else if (cor=="verde")
+        {
+            //instanciar particula3
+        }
+
+        //cor vai receber a cor atual do isqueiro, e ligar a vela
+        //1 = roxo 2 = dourado 3 = verde
+    }
+}
