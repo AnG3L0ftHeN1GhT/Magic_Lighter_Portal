ffa4a7522fb9c81f0bebe388bf2d97bbf5d9ca0e
O-Odisseu <20241ctb0180009@estudantes.ifpr.edu.br>
2026-08-11 15:06:23 -0300

    menus principais prontos

--- MLP_Unity/Assets/Scripts/MainMenuManager.cs
+++ MLP_Unity/Assets/Scripts/MainMenuManager.cs
@@ -1,19 +1,32 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+
+    [SerializeField] private String jogoPrincipal;
+    [SerializeField] private String creditos;
+    [SerializeField] private String menuPrincipal;
+
     public void Jogar()
     {
-        
+        SceneManager.LoadScene(jogoPrincipal);
     }
 
     public void AbrirCreditios()
     {
-        
+        SceneManager.LoadScene(creditos);
+    }
+
+    public void AbrirMenuPrincipal()
+    {
+        SceneManager.LoadScene(menuPrincipal);
     }
 
     public void Sair()
     {
-        
+        Debug.Log("Saindo dessa joça");
+        Application.Quit();    
     }
 }
