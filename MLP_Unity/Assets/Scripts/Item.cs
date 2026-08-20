e0a51207e30b62c8f8dfb61420ca5802fcb0c578
samuel-huntrix-fan6667-decimator-of-slug <mrjuniorbetinha@gmail.com>
2026-08-20 08:47:28 -0300

    objeto em si do isqueiro pegavel adicionado
    morte a todos

--- MLP_Unity/Assets/Scripts/Item.cs
+++ MLP_Unity/Assets/Scripts/Item.cs
@@ -7,6 +7,9 @@ public class Item : ScriptableObject
     public bool stashable;
     public bool vela;
     public bool papelDeOuro;
+
+    //esse cara só não vem pro inventario, esse bool existe p pegar o isqueiro
+    public bool ItemAdquirivel;
     public AudioClip audioClip;
     public string text;
 }
