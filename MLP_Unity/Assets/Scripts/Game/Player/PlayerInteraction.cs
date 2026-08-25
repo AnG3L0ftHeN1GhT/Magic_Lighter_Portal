fbf6bf2f0ede29877795f13eca9c625966c384e5
RicVn <20241ctb0180009@estudantes.ifpr.edu.br>
2026-08-25 15:20:17 -0300

    bloco funfa

--- MLP_Unity/Assets/Scripts/Game/Player/PlayerInteraction.cs
+++ MLP_Unity/Assets/Scripts/Game/Player/PlayerInteraction.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class PlayerInteraction : MonoBehaviour
 {
@@ -16,6 +17,9 @@ public class PlayerInteraction : MonoBehaviour
     public bool kanji3;
     public bool kanji4;
 
+    public bool clickIsPressed;
+    private Vector3 playerBase;
+    private Vector3 objetoBase;
 
     public float rayDistance;
     public float pickupSpeed;
@@ -79,6 +83,23 @@ public class PlayerInteraction : MonoBehaviour
             return;
         }
 
+        if (clickIsPressed)
+        {
+            if (PressedClickCheck())
+            {
+                if (currentInteract.item.pesado)
+                {
+                    Vector3 temp = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+                    currentInteract.transform.position = objetoBase + (-playerBase + temp);
+                }
+                return;
+            }
+            else
+            {
+                clickIsPressed = false;
+            }
+        }
+
         RaycastHit hit;
         Vector3 rayOrigin = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.5f));
 
@@ -89,6 +110,8 @@ public class PlayerInteraction : MonoBehaviour
             if (interactable != null)
             {
                 UIManager.instance.SetInteractionCursor(true);
+
+                
 
                 if (leftClick.action.WasPressedThisFrame())
                 {
@@ -159,17 +182,11 @@ public class PlayerInteraction : MonoBehaviour
                         originPosition = currentInteract.transform.position;
                         originRotation = currentInteract.transform.rotation;
                         StartCoroutine(MovingObject(currentInteract, objectViewer.position));
-                    }
+                    } inutilizado e podre; não é nescessário por enquanto. Fica aqui só caso a mecância volte, o que é improvável
                     */
-                }
-
-                if (leftClick.action.WasPressedThisFrame() || leftClick.action.WasPerformedThisFrame())
-                {
-                    currentInteract = interactable;
-                    if (currentInteract.item.pesado)
-                    {
-                        currentInteract.transform.position = objectViewer.position; // arrumar essa porra pq não pode depender do raycast hit
-                    }
+                    playerBase = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+                    objetoBase = new Vector3(currentInteract.transform.position.x, currentInteract.transform.position.y, currentInteract.transform.position.z);
+                    clickIsPressed = PressedClickCheck();
                 }
                 
             }
@@ -235,5 +252,17 @@ public class PlayerInteraction : MonoBehaviour
     void HoldingHeavyObjects()
     {
         // fazer isso segurar o negócio mesmo sem estar olhando, isso é, se tiver segurando o ckick
+    }
+
+    bool PressedClickCheck()
+    {
+        if (leftClick.action.IsPressed())
+        {
+            return true;   
+        }
+        else
+        {
+            return false;
+        }
     }
 }
